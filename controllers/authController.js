a7b111730dc51b8ce8718ca8f4914ab247e8f270
setokk <tesmtracks@gmail.com>
2023-04-14 14:46:59 +0300

    Demo json for user authentication (type doctor)

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,6 @@ const bcrypt = require('bcrypt');
 
 exports.log_in = (req, res) =>
 {
-    
+    res.json({ id: 1, type: 'doctor', name: 'please', surname: 'work', email: 'test@gmail.com', phoneNumber: '6945345678', afm: '231314155'});
 }
 
