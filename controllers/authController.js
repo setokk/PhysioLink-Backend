81e82580c5aa5003e968dd52b054c1581652d005
setokk <tesmtracks@gmail.com>
2023-04-19 19:25:09 +0300

    Fixed error when creating psf user and refactored code

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,38 +13,45 @@ exports.log_in = async (req, res) =>
     if (result.length == 0) // Didn't find a match
     {
         res.json({ message: 'Invalid credentials' });
+        return;
     }
-    else
+    
+    const role = result[0].role;
+    const id = result[0].id;
+    
+    /* If role is psf, that's all the info we need 
+        (there is no 'psf' table to get more user info from in the database) */
+    if (role == 'psf')
     {
-        const role = result[0].role;
-        const id = result[0].id;
+        res.json({ id: id, username: username, role: role });
+        return;
+    }
 
-        const userQuery = `SELECT * FROM physiolink.${role} WHERE ${role}.id = ${id};`;
-        const user = await driver.executeQuery(userQuery);
-        if (role == 'doctor')
-        {
-            res.json({ id: id,
-                username: username,
-                role: role,
-                name: user[0].name,
-                surname: user[0].surname,
-                email: user[0].email,
-                phone_number: user[0].phone_number,
-                afm: user[0].afm,
-                address: user[0].address,
-                physio_name: user[0].physio_name });
-        }
-        else if (role == 'patient')
-        {
-            res.json({ id: id,
-                username: username,
-                role: role,
-                name: user[0].name,
-                surname: user[0].surname,
-                email: user[0].email,
-                phone_number: user[0].phone_number,
-                amka: user[0].amka });
-        }
+    const userQuery = `SELECT * FROM physiolink.${role} WHERE ${role}.id = ${id};`;
+    const user = await driver.executeQuery(userQuery);
+    if (role == 'doctor')
+    {
+        res.json({ id: id,
+            username: username,
+            role: role,
+            name: user[0].name,
+            surname: user[0].surname,
+            email: user[0].email,
+            phone_number: user[0].phone_number,
+            afm: user[0].afm,
+            address: user[0].address,
+            physio_name: user[0].physio_name });
+    }
+    else if (role == 'patient')
+    {
+        res.json({ id: id,
+            username: username,
+            role: role,
+            name: user[0].name,
+            surname: user[0].surname,
+            email: user[0].email,
+            phone_number: user[0].phone_number,
+            amka: user[0].amka });
     }
 }
 
