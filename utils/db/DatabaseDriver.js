2e18fb16fd2a55554f06e3a801535f00d3d8343e
setokk <tesmtracks@gmail.com>
2023-05-11 14:30:55 +0300

    Changed database hostname alias

--- utils/db/DatabaseDriver.js
+++ utils/db/DatabaseDriver.js
@@ -1,5 +1,5 @@
 const DatabaseConnector = require('./DatabaseConnector');
 const DB_CONN_LIMIT = 5;
-const driver = new DatabaseConnector("localhost", "root", "root", DB_CONN_LIMIT);
+const driver = new DatabaseConnector("physiodb", "root", "root", DB_CONN_LIMIT);
 
 module.exports = driver;
