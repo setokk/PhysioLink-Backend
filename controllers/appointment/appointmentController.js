66d490fba0ffb4b18f721453490a5e3c486f5559
setokk <tesmtracks@gmail.com>
2023-05-16 13:00:16 +0300

    Changed add appointments

--- controllers/appointment/appointmentController.js
+++ controllers/appointment/appointmentController.js
@@ -1,17 +1,38 @@
 'use strict';
 
 const driver = require('../../utils/db/DatabaseDriver');
+const EmailFactory = require('../../utils/email/EmailFactory');
 
 exports.request_appointment = async (req, res) =>
 {
     const date = req.body.date;
     const message = req.body.message;
-    const doc_id = req.body.doc_id;
+    const doctor_id = req.body.doctor_id;
     const patient_id = req.body.patient_id;
+    const patient_name = req.body.patient_name;
+    const patient_surname = req.body.patient_surname;
+    const patient_phone_number = req.body.patient_phone_number;
 
-    const query = 'INSERT INTO physiolink.appointment (date, message, isConfirmed, doc_id, patient_id) ' +
-                `VALUES ('${date}:00', '${message}', false, ${doc_id}, ${patient_id})`;
+    await driver.executeQuery('START TRANSACTION;');
+    const query = 'INSERT INTO physiolink.appointment (date, message, isConfirmed, doctor_id, patient_id) ' +
+                    `VALUES ('${date}:00', '${message}', false, ${doctor_id}, ${patient_id})`;
     await driver.executeQuery(query);
+    await driver.executeQuery('COMMIT;');
+
+    /* We now send an email to the doctor, informing them that a new request arrived */
+    const doctor_email = await driver.executeQuery(`SELECT email FROM physiolink.doctor ` +
+                    `WHERE doctor.id = ${doctor_id}`);
+    
+    /* Split DATETIME type ('2017-07-18 16:00:00') into DATE and HOUR */
+    const splitted_date = date.split(' ');
+    EmailFactory.getInstance().sendRequestEmail(doctor_email[0].email, {
+            name: patient_name,
+            surname: patient_surname,
+            phoneNumber: patient_phone_number,
+            date: splitted_date[0],
+            hour: splitted_date[1],
+            message: message
+    });
 
     res.status(201).end();
 }
