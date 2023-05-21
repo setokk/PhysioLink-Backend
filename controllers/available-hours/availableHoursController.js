ae83fe6dd87e953fc2b9290f7e9825486c903877
setokk <tesmtracks@gmail.com>
2023-05-21 11:12:11 +0300

    Completed available hours functionality

--- controllers/available-hours/availableHoursController.js
+++ controllers/available-hours/availableHoursController.js
@@ -12,7 +12,23 @@ exports.get_hours_of_month = async (req, res) =>
             `WHERE MONTH(appointment.date) = ${month} AND appointment.doctor_id = ${doctor_id} ` +
             'GROUP BY DATE(appointment.date) ' +
             'ORDER BY appointment.date ASC;';
-    const dates = await driver.executeQuery(query);
+    const result = await driver.executeQuery(query);
 
+    for (let i = 0; i < result.length; i++)
+    {
+        let splittedStart = result[i]["start_hours"].split(",");
+        splittedStart.sort();
+        
+        let splittedEnd = [];
+        for (let j = 0; j < splittedStart.length; j++)
+        {
+            let hour = (parseInt(splittedStart[j]) + 1) % 24;
+            splittedEnd.push(hour.toString());
+        }
+        result[i]["start_hours"] = splittedStart;
+        result[i]["end_hours"] = splittedEnd;
+    }
+
+    const dates = result;
     res.status(200).json({dates});
 }
