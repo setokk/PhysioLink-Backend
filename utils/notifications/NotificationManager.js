4eda327dda1669723c276a9d5df83bb9064ebe5f
setokk <tesmtracks@gmail.com>
2023-05-25 16:48:30 +0300

    Added notification manager

---
+++ utils/notifications/NotificationManager.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const driver = require('../db/DatabaseDriver');
+
+module.exports = class NotificationManager
+{
+    async createNotification(user_id, title, message)
+    {
+        const query = 'INSERT INTO physiolink.notification ' +
+                    '(user_id, title, message) ' +
+                    `VALUES (${user_id}, '${title}', '${message}');`;
+        await driver.executeQuery(query);
+    }
+}
