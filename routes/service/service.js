4e889f9c13d9019db12a62446fd56d94865c195f
setokk <tesmtracks@gmail.com>
2023-05-31 17:25:50 +0300

    Added service edit route

--- routes/service/service.js
+++ routes/service/service.js
@@ -7,6 +7,7 @@ const serviceMiddleware = require('../../middlewares/serviceMiddleware');
 
 router.post('/create', serviceMiddleware.check_service_exists,
                 serviceController.create_service);
+router.post('/edit', serviceController.edit_service);
 router.post('/link', serviceController.link_service_to_doc);
 router.post('/delete', serviceController.delete_service_of);
 router.get('/get/:id', serviceController.get_service);
