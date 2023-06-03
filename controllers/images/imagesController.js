d0af6a4383b3edf632c5bc415b456ecaccdd4a31
setokk <tesmtracks@gmail.com>
2023-06-03 16:26:10 +0300

    Added image conversion

--- controllers/images/imagesController.js
+++ controllers/images/imagesController.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const path = require('path');
+const sharp = require('sharp');
 
 const driver = require('../../utils/db/DatabaseDriver');
 
@@ -9,35 +10,38 @@ const HOSTNAME = 'http://physiolink.chibo.uk';
 exports.get_image = async (req, res) =>
 {
     const user_id = req.params.user_id;
- 
-    const user = await driver.executeQuery('SELECT user.image FROM physiolink.user ' +
-            `WHERE user.id=${user_id};`);
-    
-    
-    res.sendFile(user[0].image);
+    res.sendFile(path.join(process.cwd(), `/images/profile/${user_id}.jpeg`));
 }
 
 exports.upload_image = async (req, res) =>
 {
     const user_id = req.params.user_id;
-    const image = req.files.image;
+    let image = req.files.image;
 
-    const filePath = path.join(process.cwd(), '/images/profile/') + user_id + "." +
-        image.name.split(".")[1];
+    const filePath = path.join(process.cwd(), '/images/profile/') + user_id + ".jpeg";
     console.log(filePath);
 
-    image.mv(filePath, async (err) => {
-        if (err) {
-          console.error('Error saving image:', err);
-          res.sendStatus(500);
-          return;
-        }
+    sharp(image.data)
+        .toFormat('jpeg')
+        .toBuffer()
+        .then((convertedImage) =>
+        {
+            image.data = convertedImage;
+            
+            image.mv(filePath, async (err) => {
+                if (err) {
+                  console.error('Error saving image:', err);
+                  res.sendStatus(500);
+                  return;
+                }
+                
+                await driver.executeQuery('UPDATE physiolink.user ' + 
+                    `SET user.image='${HOSTNAME + '/physiolink/api/images/get/'}${user_id}' ` +
+                    `WHERE user.id=${user_id}`);
         
-        await driver.executeQuery('UPDATE physiolink.user ' + 
-            `SET user.image='${HOSTNAME + '/physiolink/api/images/get/'}${user_id}' ` +
-            `WHERE user.id=${user_id}`);
+                console.log('Image saved:', filePath);
+                res.sendStatus(200);
+              });
 
-        console.log('Image saved:', filePath);
-        res.sendStatus(200);
-      });
+        });
 }
