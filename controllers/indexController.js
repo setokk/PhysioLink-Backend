b27291967d90451df91fe727abd24bb961434cf1
setokk <tesmtracks@gmail.com>
2023-06-12 13:28:42 +0300

    Added mobile documentation

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -6,3 +6,8 @@ exports.show_documentation = (req, res) =>
 {
     res.sendFile(path.join(__dirname, '../documentation/documentation.html'));
 }
+
+exports.show_android_documentation = (req, res) =>
+{
+    res.sendFile(path.join(__dirname, '../documentation/mobiile-documentation.html'));
+}
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,6 @@ const indexController = require('../controllers/indexController.js');
 
 /* GET documentation page. */
 router.get('/', indexController.show_documentation);
+router.get('/android', indexController.show_android_documentation);
 
 module.exports = router;    
